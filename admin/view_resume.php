4a2b5bf8fe51e36cce2c417e718b0b7d6bdc30e8
Misbah <misbahiqbal147@gmail.com>
2025-08-28 09:47:20 -0700

    update code

--- admin/view_resume.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "session.php";
-include_once "header.php";
-$id=$_REQUEST['id'];
-$sql="select * from student_profile where student_id='$id'";
-$result=mysqli_query($con,$sql);
-$row=mysqli_fetch_array($result);
-?>
-<embed src="../resume/<?php echo $row['resume'];?>" type="application/pdf" width="100%" height="600px" />
